0c0dbe728425318672cf655877ea1c0922d6857e
anshulp19 <ianshulpandey19@gmail.com>
2020-11-10 02:18:04 +0530

    UI for data sent from kafka stream

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ws = new WebSocket('ws://localhost:3210');
+
+class App extends Component {   
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+  componentWillMount() {
+    ws.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+
+    ws.onmessage = (message) => {
+      const info = this.state.data;
+      info.push(JSON.parse(message.data));
+      this.setState({
+        data: info,
+      });
+    };
+  }
+
+  componentDidMount() {
+  }
+  
+  render() {
+    console.log("data: " + JSON.stringify(this.state.data));
+    return (
+      this.state.data.length === 0 ? (<span> Fetching</span>)
+      : (
+      <LineChart
+        width={500}
+        height={300}
+        data={this.state.data}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="factoryID" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="persons" stroke="#8884d8" activeDot={{ r: 8 }} />
+      </LineChart>
+      )
+    );
+  }
 }
 
 export default App;
